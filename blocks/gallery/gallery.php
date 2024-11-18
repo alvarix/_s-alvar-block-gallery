f7e32969f9b8d1517ff8774bbccf9420b5bd62cc
Alvar Sirlin <alvar@alvarsirlin.com>
2024-11-18 16:29:42 -0500

    guess what

--- blocks/gallery/gallery.php
+++ blocks/gallery/gallery.php
@@ -40,7 +40,7 @@ if ($posts):
         if(get_field('dont_use_thumb') != false) {
             $thumb = get_field('thumb', $post->ID);
         }
-        if (!$thumb || get_field('dont_use_thumb') == true) {
+        if (!$thumb || get_field('dont_use_thumb') == false) {
             $thumb = get_the_post_thumbnail_url($post->ID, 'thumbnail');
         }
     ?>
