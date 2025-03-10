eed53c69a64fa953744ff703a7a3f89c0010ae19
Alvar Sirlin <alvar@alvarsirlin.com>
2025-03-10 14:36:10 -0400

    email signup ewmromo

--- _mc-signup-form.php
+++ _mc-signup-form.php
@@ -1,7 +1,7 @@
 <div id="mc_embed_signup">
     <form action="https://nyc.us9.list-manage.com/subscribe/post?u=e70c5037279d21943c158f3fa&amp;id=2b53bbbdcd&amp;f_id=00bdc2e0f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
         <div id="mc_embed_signup_scroll">
-            <div class="mc-field-group"><input type="email" placeholder="Newsletter" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></div>
+            <div class="mc-field-group"><input type="email" placeholder="20% off w Signup!" name="EMAIL" class="required email" id="mce-EMAIL" required="" value=""></div>
         </div>
         <div id="mce-responses" class="clear foot">
             <div class="response" id="mce-error-response" style="display: none;"></div>
@@ -13,7 +13,7 @@
         </div>
         <div class="optionalParent">
             <div class="clear foot btn">
-                <input type="submit" name="subscribe" id="mc-embedded-subscribe"  value="Sign Me Up!">
+                <input type="submit" name="subscribe" id="mc-embedded-subscribe"  value="Newsletter">
             </div>
         </div>
     </form>
